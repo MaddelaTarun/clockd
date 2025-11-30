50fba8ac78b6ee0634898548baafd2c7bfc86477
Tarun <maddelatarun0@gmail.com>
2025-12-01 00:20:44 +0530

    feature: added a 12h toggle

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const sun = document.getElementById("icon-sun");
 const moon = document.getElementById("icon-moon");
 const themeButton = document.getElementById("theme");
 
+const formatToggle = document.getElementById("format-toggle");
+const secondsToggle = document.getElementById("seconds-toggle")
+
+let is24Hour = true;
+let showSeconds = true;
+
 themeButton.addEventListener("click", ()=> {
     document.body.classList.toggle("dark");
 
@@ -14,12 +20,39 @@ themeButton.addEventListener("click", ()=> {
 function updateClock() {
 
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2,0);
-    const minutes = now.getMinutes().toString().padStart(2,0);
-    const seconds = now.getSeconds().toString().padStart(2,0);
-    const timeString = `${hours}:${minutes}:${seconds}`
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    let period = "";
+    if(!is24Hour) {
+        period = hours >= 12 ? "PM":"AM";
+        hours = hours % 12 || 12;
+    }
+
+    hours = hours.toString().padStart(2,"0");
+    minutes = minutes.toString().padStart(2,"0");
+    seconds = seconds.toString().padStart(2,"0");
+
+    let timeString = `${hours}:${minutes}`
+
+    if(showSeconds) {
+        timeString += `:${seconds}`
+    }
+
+    if(!is24Hour) {
+        timeString += ` ${period}`
+    }
     document.getElementById("clock").textContent = timeString;
 }
+
+formatToggle.addEventListener("click", () => {
+    is24Hour = !is24Hour;
+
+    formatToggle.textContent = is24Hour ? "[ 12h ]" : " [ 24h ]"
+
+    updateClock();
+})
 
 updateClock();
 setInterval(updateClock,1000)
