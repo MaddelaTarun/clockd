0c916414e2d97533b3615c3c27b67df1254c1d5b
Maddela Tarun <maddelatarun0@gmail.com>
2025-12-02 17:18:36 +0530

    minor: modified/organized html

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const moon = document.getElementById("icon-moon");
 const themeButton = document.getElementById("theme");
 
 const formatToggle = document.getElementById("format-toggle");
-const secondsToggle = document.getElementById("seconds-toggle")
+const secondsToggle = document.getElementById("seconds-toggle");
+const navLinks = document.querySelectorAll("#nav-center a");
+const screens = document.querySelectorAll(".screen");
+const timerMessage = document.getElementById("timer-message");
+const stopWatchMessage = document.getElementById("stopwatch-message");
 
 let is24Hour = true;
 let showSeconds = true;
@@ -45,6 +49,51 @@ function updateClock() {
     }
     document.getElementById("clock").textContent = timeString;
 }
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const screenName = link.dataset.screen;
+        const targetScreen = document.getElementById(`screen-${screenName}`);
+        if(!targetScreen) return;
+
+        screens.forEach((screen) => {
+            screen.style.display = "none";
+        })
+
+        targetScreen.style.display = "block";
+
+        if(screenName === "timer" && timerMessage) {
+            timerMessage.textContent = "still in progress";
+        }
+
+        if(screenName === "stopwatch" && stopWatchMessage) {
+            stopWatchMessage.textContent = "still in progress";
+        }
+    })
+})
+
+
+// navLinks.forEach((link) => {
+//     link.addEventListener("click", (event) => {
+//       event.preventDefault();
+  
+//       const screenName = link.dataset.screen;
+//       const targetScreen = document.getElementById(`screen-${screenName}`);
+//       if (!targetScreen) return;
+  
+//       // hide all screens
+//       screens.forEach((screen) => {
+//         screen.style.display = "none";
+//       });
+  
+//       // show the selected screen
+//       targetScreen.style.display = "block";
+  
+//       // when timer is selected, show "still in progress"
+//       if (screenName === "timer" && timerMessage) {
+//         timerMessage.textContent = "still in progress
 
 formatToggle.addEventListener("click", () => {
     is24Hour = !is24Hour;
